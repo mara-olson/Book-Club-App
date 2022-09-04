8f451afac311095b4589671081137ec1144ed2b7
mara-olson <mara.c.olson@gmail.com>
2022-09-04 17:27:14 -0500

    Javascript src fixed in base.html and static folder added

--- main.js
+++
@@ -1,9 +0,0 @@
-const addBookBtn = document.querySelector("#add-book-btn");
-
-console.log("addBook");
-
-function addBook() {
-  alert("Add a book?");
-}
-
-addBookBtn.addEventListener("click", addBook);
