5ca76df14e2b7bd5592d800d84cf9852ca6a2485
mara-olson <mara.c.olson@gmail.com>
2022-09-03 19:50:33 -0500

    main.js added but non-functional

---
+++ main.js
@@ -0,0 +1,9 @@
+const addBookBtn = document.querySelector("#add-book-btn");
+
+console.log("addBook");
+
+function addBook() {
+  alert("Add a book?");
+}
+
+addBookBtn.addEventListener("click", addBook);
