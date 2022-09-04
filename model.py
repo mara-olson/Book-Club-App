51df6d1fbdd1f78dacf2a5b5a39855ecfa04675a
mara-olson <mara.c.olson@gmail.com>
2022-09-03 19:00:55 -0500

    Starter html file and homepage app route added

--- model.py
+++ model.py
@@ -12,7 +12,9 @@ class Book(db.Model):
     year_published = db.Column(db.Integer)
     date_read = db.Column(db.DateTime)
     rating = db.Column(db.Integer)
-    note_ids = db.Column(db.Array)
+    # note_ids = db.Column(db.ARRAY)
+
+    notes = db.relationship("Note", back_populates="book")
 
     def __repr__(self):
         return f'<Book id={self.book_id} title={self.title}'
@@ -28,6 +30,29 @@ class Note(db.Model):
     category = db.Column(db.String)
     quote = db.Column(db.Boolean)
 
+    book = db.relationship("Book", back_populates="notes")
+    
+
     def __repr__(self):
         return f'<Note id={self.note_id} content={self.content[:8]}'
+
+def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
 
