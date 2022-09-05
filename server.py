b6d82a246c30ed9565247fcd5876abbafe348466
mara-olson <mara.c.olson@gmail.com>
2022-09-04 19:19:00 -0500

    API for add-book added in server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import (Flask, render_template, request, flash, session, redirect)
-from model import connect_to_db, db
+from flask import Flask, render_template,json, jsonify, request, flash, session, redirect;
+from model import connect_to_db, db, Book, Note
 from datetime import datetime
 import crud
 
@@ -13,8 +13,23 @@ app.jinja_env.undefined = StrictUndefined
 @app.route("/")
 def homepage():
     """Landing page for bookclub app."""
-
+    
     return render_template('base.html')
+
+@app.route("/add-book", methods=["POST"])
+def add_book():
+    """Add a new book to the database."""
+    data = request.json
+    new_book_title = data.get("new_book_title")
+    new_book_author = data.get("new_book_author")
+    new_book_year = data.get("new_book_year")
+    new_book_date_read = data.get("new_book_date_read")
+    new_book_rating = data.get("new_book_rating")
+
+    new_book = Book.create_book(title=new_book_title, author=new_book_author, year_published=new_book_year, date_read=new_book_date_read, rating=new_book_rating)
+
+    return jsonify({"book-title": new_book.title})
+
 
 
 if __name__ == "__main__":
