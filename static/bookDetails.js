f20ee9af90b3a34036877908969e17bb6f634536
mara-olson <mara.c.olson@gmail.com>
2022-11-21 19:39:35 -0800

    Replaced class as initializer selector for tinymce with specific id for content-notes

--- static/bookDetails.js
+++ static/bookDetails.js
@@ -6,38 +6,22 @@ const setNoteContent = () => {
   const tinyEditor = document.getElementById("edit-content-notes-tiny");
 
   const tinyEditorContent = tinyEditor.value;
-  console.log(tinyEditorContent);
+  // console.log(tinyEditor);
 
   const quoteContent = document.getElementById("quotes-notes-list");
 
   const vocabContent = document.getElementById("vocab-notes-list");
 
-  const getTinyContent = () => {
-    if (!tinyEditor.getContent()) {
-      tinyEditor.setContent("Click to add notes");
-    } else {
-      tinyEditor.getContent();
-    }
-  };
-
   fetch(`/api/library/${book_title}/all-notes`)
     .then((response) => response.json())
     .then((data) => {
-      console.log("DONE");
-      // if ((data.notes = null)) {
-      //   console.log(data.notes);
-      // } else {
-      // console.log(tinyContent);
-      if (tinyEditor.value == null) {
-        tinyEditor.value = "Click to add notes";
-      } else {
+      if (data.notes[0].category === "content") {
         tinyEditor.value = data.notes[0].content;
+      } else {
+        tinyEditor.value = "Click to add notes";
       }
-      console.log(data.notes[0].content);
-
-      // }
-      // tinyEditor.setContent(tinyContent);
+      console.log(data.notes[0].category);
     });
 };
 
-noteContent.addEventListener("click", setNoteContent, false);
+noteContent.addEventListener("click", setNoteContent);
